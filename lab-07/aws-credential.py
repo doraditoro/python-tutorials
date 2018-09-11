92b9b1589697cc1e180d1fbe2d627871a0c31bfe
localadmin <doraditoro@gmail.com>
2018-09-11 15:24:32 -0500

    aws-credential

---
+++ lab-07/aws-credential.py
@@ -0,0 +1,4 @@
+[default]
+aws_access_key_id = AKIAJSCTORPRAA7B4VPA
+aws_secret_access_key = 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
+region = us-east-1
--- lab-07/ec-create-instance.py
+++ lab-07/ec-create-instance.py
@@ -3,9 +3,9 @@ import boto3
 
 def create_instance():
     ec2_resource = boto3.resource('ec2')
-    instances = ec2_resource.create_instances(ImageId='ami-49f0762d',
+    instances = ec2_resource.create_instances(ImageId='ami-6871a115)',
                 MinCount=1, MaxCount=3,InstanceType='t2.micro',
-                SecurityGroupIds=['ansible-node'],KeyName='ansible')
+                SecurityGroupIds=['launch-wizard-5'],KeyName='fullstack')
     instance_ids = []
     for instance in instances:
         instance_ids.append(instance.id)
