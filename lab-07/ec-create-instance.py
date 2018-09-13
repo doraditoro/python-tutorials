46b18bf19ad9f5c4311b33f7d3354f8b19d43b58
localadmin <doraditoro@gmail.com>
2018-09-13 14:25:59 -0500

    create instance fixed

--- lab-07/ec-create-instance.py
+++ lab-07/ec-create-instance.py
@@ -2,15 +2,8 @@ import boto3
 
 
 def create_instance():
-<<<<<<< HEAD
     ec2_resource = boto3.resource('ec2')
-    instances = ec2_resource.create_instances(ImageId='ami-6871a115)',
-=======
-    
-    ec2_resource = boto3.resource('ec2', region_name='us-east-1')
-    
     instances = ec2_resource.create_instances(ImageId='ami-6871a115',
->>>>>>> 814d7a14c70ff1674856c79aea58d06aa664815b
                 MinCount=1, MaxCount=3,InstanceType='t2.micro',
                 SecurityGroupIds=['launch-wizard-5'],KeyName='fullstack')
     instance_ids = []
