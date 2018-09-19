8d58643679c35aa33fb1bf7c4f9b0d9595aac464
localadmin <doraditoro@gmail.com>
2018-09-19 13:47:07 -0500

    accessnkey changed

--- lab-07/aws-credential.py
+++ lab-07/aws-credential.py
@@ -1,4 +1,4 @@
 [default]
-aws_access_key_id = AKIAJSCTORPRAA7B4VPA
-aws_secret_access_key = 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
+aws_access_key_id = AKIAIKBNXJTA3AQYBS7Q
+aws_secret_access_key = 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
 region = us-east-1
