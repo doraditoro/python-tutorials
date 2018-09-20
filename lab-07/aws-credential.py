23c67a4a9f0f671e74c79960f8f55d31186fab5d
localadmin <doraditoro@gmail.com>
2018-09-20 07:19:08 -0500

    remove access key

--- lab-07/aws-credential.py
+++ lab-07/aws-credential.py
@@ -1,5 +1,5 @@
 [default]
 
-aws_access_key_id = AKIAIVCKNFHY43VPJDQA
-aws_secret_access_key = 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 
+aws_access_key_id = 
+aws_secret_access_key = 
 region = us-east-1
